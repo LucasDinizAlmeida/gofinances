b51f3f7fc15558cb0f1b6a4dea5707095d73b40d
Lucas Diniz Almeida <almeidalucas820@gmail.com>
2022-08-22 14:40:28 -0300

    first commit and installing from styled-components, adding typescript, creating the first screen Dashboard.

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react';
+import { Text } from 'react-native';
+import {
+  Container, Header
+} from './styles';
+
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <Text>Header</Text>
+      </Header>
+    </Container>
+  );
+}
